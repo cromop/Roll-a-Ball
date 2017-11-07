e644637dfa33791fae98825edc3340755e2e6f6f
cromo <cromo@ugr.es>
2017-11-07 14:02:35 +0100

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed; // si es public aparece en el editor
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called before rendering a frame
+    void Update () {
+		
+	}
+
+    // Fixed Update is called before perform any physics calculations
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+            other.gameObject.SetActive(false);
+    }
+}
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+Roll-a-ball tutorial provided by Unity
+
+https://unity3d.com/es/learn/tutorials/s/roll-ball-tutorial
