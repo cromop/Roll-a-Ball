3ab7766ce09e2fd521cc1868bd0c5f41b20f17b4
cromo <cromo@ugr.es>
2017-11-10 14:04:50 +0100

    added Gear VR touchpad input and accelerometer input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,39 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed; // si es public aparece en el editor
+    public float speed; // if the variable is public, it appears in the editor
+	public Text countText;
+	public Text winText;
 
     private Rigidbody rb;
+	private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+		count = 0;
+		winText.text = "";
+		setCountText();
     }
 
     // Update is called before rendering a frame
-    void Update () {
-		
+    void Update ()
+	{
+		switch (SystemInfo.deviceType) {
+		case DeviceType.Desktop:
+			// Exit condition for Desktop devices
+			if (Input.GetKey ("escape"))
+				Application.Quit ();
+			break;
+		case DeviceType.Handheld:
+			// if (XRSettings.isDeviceActive)
+			if (XRDevice.isPresent) {
+			} else {
+				// Exit condition for mobile devices
+				if (Input.GetKeyDown (KeyCode.Escape))
+					Application.Quit ();    
+			}
+			break;
+		}
 	}
 
     // Fixed Update is called before perform any physics calculations
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+		float moveHorizontal, moveVertical;
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		switch (SystemInfo.deviceType) 
+		{
+			case DeviceType.Desktop:
+				moveHorizontal = Input.GetAxis ("Horizontal");
+				moveVertical = Input.GetAxis ("Vertical");
+			break;
+			case DeviceType.Handheld:
+//				if (XRSettings.isDeviceActive)
+				if (XRDevice.isPresent)
+				{
+					moveHorizontal = Input.GetAxis ("Mouse X");
+					moveVertical = Input.GetAxis ("Mouse Y");
+				}
+				else
+				{
+					moveHorizontal = Input.acceleration.x;
+					moveVertical = Input.acceleration.y;
+				}
+			break;
+			default:
+				moveHorizontal = 0;
+				moveVertical = 0;
+			break;
+		}
 
-        rb.AddForce(movement * speed);
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed * 2);
     }
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Pick Up"))
-            other.gameObject.SetActive(false);
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			setCountText();
+		}
     }
+
+	void setCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+		if (count >= 12)
+			winText.text = "You win!";	
+	}
 }
 
 
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Roll-a-ball tutorial provided by Unity
+Extended version of the Roll-a-ball tutorial provided by Unity: https://unity3d.com/es/learn/tutorials/s/roll-ball-tutorial
 
-https://unity3d.com/es/learn/tutorials/s/roll-ball-tutorial
+The game works on Desktop devices (through the keyboard) and mobile devices (using the accelerometer). It can be also run on a Gear VR, using the touchpad to control de ball.
